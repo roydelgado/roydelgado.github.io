eb1077e1aafa51bef6a8ab4aa4fcb89cac4e5afc
roydelgado <koding@roydelgado.com>
2021-01-30 21:17:53 +0000

    Deploying to gh-pages from  @ 7be2456c9cbf0ab0724439f94286e8428ae44dab 🚀

--- _next/static/RvNxgxnIjUbHGY7FzPAv-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-c8a3fad2957888d6b60b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-dce9aee01ae6da044fd7.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
